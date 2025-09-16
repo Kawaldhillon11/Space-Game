5f955c54bf76ae9317e95fea90138406cb6583b7
kawal <sin10145@sheridancollege.ca>
2025-09-16 17:18:26 -0400

    created Characters class in characters module

---
+++ characters.py
@@ -0,0 +1,6 @@
+class Character:
+    def __init__(self, name, strength, hacking):
+        self.name = name
+        self.energy = 10
+        self.strength = strength
+        self.hacking = hacking
---
+++ main.py
@@ -0,0 +1,5 @@
+import characters
+
+def game():
+    andy = characters.Character("Andy", 100, 10)
+    dr = characters.Character("Dr. Aris", 10, 100)
