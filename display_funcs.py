864cbe4dd5bab59f21dba32407809b47850c70dd
kawal <kawaldhillon5@gmail.com>
2025-09-16 13:25:55 -0400

    created display_funcs module

--- display_funcs.py
+++ display_funcs.py
@@ -1,12 +1,8 @@
 import time
 
-str = "Hi this is a test"
-
-
 def slow_print(str):
     for  char in str:
         print(char, end="", flush= True)
         time.sleep(0.1)    
     print()
 
-slow_print(str)
