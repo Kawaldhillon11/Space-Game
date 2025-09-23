294d467b5da6d2d02d96294d89bb4c117a0330c4
kawal <sin10145@sheridancollege.ca>
2025-09-22 23:38:11 -0400

    created challenge setup functions

--- challenges.py
+++ challenges.py
@@ -19,8 +19,9 @@ def determine_sequence(random_list : list, correct_list: list):
                 index = index + 1
     return seq
 
-def breaching_ship(choice):
-    print(choice.name)
+def breaching_ship(choose_func):
+    challenge_intro = "The Ships Docking Mechanism have been Damaged and cannot be Used, We need to find another way of Entering the Ship, If you are Up to the Challeneg One of you could try to Brake Open the Entrance Door."
+    choice = choose_func(intro = challenge_intro)
 
     door_structural_integrity = 180
     while (choice.energy and (door_structural_integrity >= 0)):
@@ -28,18 +29,24 @@ def breaching_ship(choice):
         door_structural_integrity = door_structural_integrity - choice.hit()
         choice.energy = choice.energy -1
         print(f"energy left {choice.energy}")
-        print(f"door integrrity {door_structural_integrity}")
+        print(f"door integrrity {int(door_structural_integrity)}")
 
     if(choice.energy == 0):
         return False
     elif (door_structural_integrity <= 0):
         return True
     
-
-def restore_power(choice):
-
+def restore_power_setup(choose_func):
+    time.sleep(2)
+    challenge_intro = "Nice Job! You have Entered the Ship, now we need to Restore Power so that Required Data could be Extracted, To Restore Power we need to execute some steps in correct order, on the screen you will see jumbled steps due to damage to the ship, the Time for which the steps will remain on the screen depends on your hacking abilities, This Challenge will use 4 units of Energy"
     clear_screen()
-    print(choice.name)
+    choice = choose_func(intro = challenge_intro)
+
+    if(choice.energy < 5):
+        slow_print("Character Does not Have enough energy")
+        return False
+    choice.energy -= 4 
+
     slow_print("Please use following steps in correct ordre to Restore Power ")
     restore_power_steps = [
         "Open Acess Pannel",
@@ -47,7 +54,6 @@ def restore_power(choice):
         "Enter Password",
         "Reboot System"
     ]
-    password = "Admin"
     randomized_steps = random.sample(restore_power_steps, k= len(restore_power_steps))
 
     for index, step in enumerate(randomized_steps, start=1):
@@ -55,28 +61,59 @@ def restore_power(choice):
 
     time.sleep(choice.hacking/10)
     clear_screen()
-    if ((input("Enter Correct Sequence(Example: 1234) : ")) == determine_sequence(randomized_steps, restore_power_steps)):
+    entered_sequence = input("Enter Correct Sequence(Example: 1234) : ")
+    slow_print("Checking Sequence")
+    if restore_power(choice,restore_power_steps, randomized_steps, entered_sequence):
+        slow_print("Sequence Matched")
         return True
-    else: 
+    else:
+        slow_print("Sequence Mismatch")
+        return False
+    
+
+
+
+def restore_power(choice, restore_power_steps, randomized_steps, entered_sequence):
+
+    return (entered_sequence == determine_sequence(randomized_steps, restore_power_steps))
+        
+
+def extract_data_setup(choose_func):
+    time.sleep(2)
+    clear_screen()
+    challenge_intro = "Great! You are now Entering Data Storage of the Ship, The Storage has 10 Sections and the data we are looking for could be in any one of the Sections, Looking into each Section uses 1 Energy unit, So choose accordingly"
+    choice  = choose_func(intro = challenge_intro)
+    if(choice.energy < 1):
+        slow_print("Character Does not Have enough energy")
+        return False
+    slow_print(f"You can choose to look into {choice.energy} Sections")
+    sections_to_look = input(f"Please Enter Maximum {choice.energy} Section numbers (e.g 12345) : ")
+    if(sections_to_look.isnumeric()):
+        sections_to_look_sanitized = sections_to_look[0:choice.energy]
+        if extract_data(sections_to_look_sanitized,  choice):
+            slow_print("Data Found!")
+            return True
+        else:
+            slow_print("Data not Found")
+    else:
+        slow_print("Invalid Section number entered")
         return False
 
 
-
-def extract_data(sections_to_look):
+def extract_data(sections_to_look, choice):
     data_section = random.randint(0,9)
     data_sections_list = []
     for x in range(10):
         data_sections_list.append(x == data_section)
     
-    print(data_sections_list)
-
     for index in sections_to_look:
-        if(data_sections_list[index]== True):
+        choice.energy -= 1
+        if(data_sections_list[int(index)]== True):
             return True
         
     return False
     
     
 
-
-print(extract_data([2,3,4,7,8,9]))
+# restore_power_setup(tubi)
+# extract_data_setup(tubi)
--- main.py
+++ main.py
@@ -3,34 +3,49 @@ import challenges
 from display_funcs import slow_print
 from display_funcs import clear_screen
 
-def choose_character(character1, character2):
-    slow_print(f"1. {character1.name}")
-    slow_print(f"2. {character2.name}")
-
-    while True:
-        choice = int(input("Choose a Character: "))
-        if(choice == 1 or choice == 2):
-            if(choice == 1):
-                return character1
-            else:
-                return character2
 
 def game():
 
-    andy = characters.Character("Andy", 100, 10)
-    dr = characters.Character("Dr. Aris", 10, 100)
+    andy = characters.Character("Andy", 100, 150)
+    dr = characters.Character("Dr. Aris", 10, 150)
 
-    challeneges_list = [challenges.breaching_ship, challenges.restore_power, challenges.extract_data]
+    def choose_character(character1 = andy , character2 = dr, intro = ""):
+        slow_print(intro)
+        slow_print("Choose One")
+        slow_print(f"1. {character1.name}, Energy: {character1.energy}, Hacking: {character1.hacking}, Strength: {character1.strength}")
+        slow_print(f"2. {character2.name}, Energy: {character2.energy}, Hacking: {character2.hacking}, Strength: {character2.strength}")
 
-    welcome_msg = "Welcome to Space Game"
+        while True:
+            choice = int(input("Choose a Character: "))
+            if(choice == 1 or choice == 2):
+                if(choice == 1):
+                    return character1
+                else:
+                    return character2
+            else:
+                print("Invalid")
+
+    challeneges_list = [challenges.breaching_ship, challenges.restore_power_setup, challenges.extract_data_setup]
+
+    welcome_msg = "Welcome to Top Secret Mission, One of our Space Discovery ships got Stranded in Space near Mars, We think they might have been Attcked, They were comming back with valuable data, Your Mission is to Extract the Data and look for any Survivors"
     clear_screen()
     slow_print(welcome_msg)
 
     for challenge in challeneges_list:
-        choice = choose_character(andy, dr)
-        if(challenge(choice)):
-            continue
-        else: 
-            break
+        running = True
+        while running:
+            if not (challenge(choose_character)):
+                if (input("Try Again (Y/N) ?").casefold() == "n"):
+                    running = False
+                    return False
+            else:
+                slow_print("Challenge Passed!")
+                running = False 
+    return True
+    
+                
 
-game()
+if game():
+    slow_print("Great Job! We Thank you both for your Service.")
+else:
+    slow_print("Quest Failed!")
